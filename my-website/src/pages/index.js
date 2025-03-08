a1f9d64bfc722820ee62e8cef7df05e631cc6c22
mo-xiaoxiu <1807115463@qq.com>
2025-03-08 18:42:09 +0800

    personal modify

--- my-website/src/pages/index.js
+++ my-website/src/pages/index.js
@@ -19,8 +19,8 @@ function HomepageHeader() {
         <div className={styles.buttons}>
           <Link
             className="button button--secondary button--lg"
-            to="/docs/intro">
-            Docusaurus Tutorial - 5min ⏱️
+            to="/blog/一手代码 一手墨迹">
+            开始浏览 🐣
           </Link>
         </div>
       </div>
